f4431afcbc5ae8a6c14554a66d320dd2839a4cad
mo214885 <mo214885@falmouth.ac.uk>
2019-11-22 17:51:06 +0000

    Added unity project with effects and a punching animation
    Signed-off-by: mo214885 <mo214885@falmouth.ac.uk>

---
+++ ShaderPrototype280/Assets/Scripts/ProjectileMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileMovement : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+
+    float z;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        z = transform.position.z + 1 * Time.deltaTime;
+        transform.position = new Vector3(transform.position.x, transform.position.y, z);
+
+        Destroy(gameObject, 5);
+    }
+}
