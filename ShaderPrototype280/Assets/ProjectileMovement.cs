044e99485d5a0ad9c35e11cebf85efd748d1e5bf
mo214885 <mo214885@falmouth.ac.uk>
2019-11-26 15:13:50 +0000

    Added lava material and terrain
    Signed-off-by: mo214885 <mo214885@falmouth.ac.uk>

---
+++ ShaderPrototype280/Assets/ProjectileMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileMovement : MonoBehaviour
+{
+    [SerializeField]
+    float height;
+
+    Rigidbody rb;
+    float z;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(0.0f, 0.0f, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y <= -2)
+        {
+            Debug.Log(rb.velocity);
+            rb.AddForce(new Vector3(0, height, 0), ForceMode.Impulse);
+            rb.velocity = new Vector3(0.0f, 0.0f, 0.0f);
+        }
+    }
+}
