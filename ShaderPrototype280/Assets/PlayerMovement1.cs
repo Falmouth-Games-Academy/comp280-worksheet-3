59a05ac36a488c468e250a25fba3effa5b335d8e
mo214885 <mo214885@falmouth.ac.uk>
2019-11-28 15:30:33 +0000

    Removed cinemachine and added lava material
    Signed-off-by: mo214885 <mo214885@falmouth.ac.uk>

---
+++ ShaderPrototype280/Assets/PlayerMovement1.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement1 : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+
+    float x;
+    float y;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        y = -rb.mass;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector3(0, y, Input.GetAxis("Horizontal") * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            StartCoroutine(Jump());
+    }
+
+    public IEnumerator Jump()
+    {
+        y = jumpForce;
+        yield return new WaitForSeconds(1);
+        y = -rb.mass;
+    }
+}
